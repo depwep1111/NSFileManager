e8a857b05231d4b641ce1a19a4b47d84f57b0701
tran thanh <trantrungthanh@trans-Mac.local>
2017-05-17 23:52:21 +0700

    Initial Commit

---
+++ System/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  System
+//
+//  Created by tran trung thanh on 5/17/17.
+//  Copyright © 2017 tran trung thanh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
